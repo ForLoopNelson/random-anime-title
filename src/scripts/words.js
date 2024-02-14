cd56813c96646403c0db2c4478c9dde24f47024c
Cory Nelson <fullnelsontech@gmail.com>
2024-02-14 15:17:57 -0500

    first commit. contains components, pages, styles ect

---
+++ src/scripts/words.js
@@ -0,0 +1,26 @@
+export var fonts = ["romanceFatal"]
+
+export var fist = [
+  "The",
+  "My",
+  "His",
+  "Her",
+  "Their",
+  "A",
+  "It's",
+  "This",
+  "That",
+]
+
+export var second = [
+  "Senpai",
+  "Kohai",
+  "Magical",
+  "Sister",
+  "Friends",
+  "Adventure",
+]
+
+export var third = ["The", "of", "about", "from", "is"]
+
+export var fourth = ["World", "Cute", "Dangers", "Dungeon", "Castle", "Journey"]
